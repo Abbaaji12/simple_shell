865393dc43771d1c206bc4ec0a5c60569a3c46cb
root <Abbaaji12@gmail.com>
2023-10-13 15:17:29 -0700

    simple_shell

---
+++ get_line.c
@@ -0,0 +1,66 @@
+#include "shell.h"
+
+/**
+* _getline - Read The Input By via from Stdin
+* This function reads a line of user input from the standard input (stdin).
+* Return: A pointer to the input string read from the user.
+*/
+char *_getline()
+{
+int i, buffsize = BUFSIZE, rd;
+char c = 0;
+char *buff = malloc(buffsize);
+
+	if (buff == NULL)
+	{
+		free(buff);
+		return (NULL);
+	}
+
+	for (i = 0; c != EOF && c != '\n'; i++)
+	{
+		fflush(stdin);
+		rd = read(STDIN_FILENO, &c, 1);
+		if (rd == 0)
+		{
+			free(buff);
+			exit(EXIT_SUCCESS);
+		}
+		buff[i] = c;
+		if (buff[0] == '\n')
+		{
+			free(buff);
+			return ("\0");
+		}
+		if (i >= buffsize)
+		{
+			buff = _realloc(buff, buffsize, buffsize + 1);
+			if (buff == NULL)
+			{
+				return (NULL);
+			}
+		}
+	}
+	buff[i] = '\0';
+	hashtag_handle(buff);
+	return (buff);
+}
+
+/**
+ * hashtag_handle - Remove everything after the '#' character in a string.
+ * @buff: Input string.
+ * Return: void (no return value).
+ */
+void hashtag_handle(char *buff)
+{
+	int i;
+
+		for (i = 0; buff[i] != '\0'; i++)
+		{
+			if (buff[i] == '#')
+			{
+			buff[i] = '\0';
+			break;
+			}
+	}
+}
