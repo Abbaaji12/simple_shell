686b7213019c4fe5c75a47fe9340a12f07eb08cf
Abbaaji12 <mohammedajimustapha@gmail.com>
2023-10-15 16:08:23 -0700

    promptfun parssing files were added

---
+++ parssing.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * parse_cmd - it Parse Line Of an Input
+ * @input:a User Input To  be Parse
+ * Return:An Array Of Character to be (Parsed):Simple Shell
+ */
+char **parse_cmd(char *input)
+{
+	char **tokens;
+	char *token;
+	int i, buffsize = BUFSIZE;
+
+	if (input == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * buffsize);
+	if (!tokens)
+	{
+		perror("hsh");
+		return (NULL);
+	}
+
+	token = _strtok(input, "\n ");
+	for (i = 0; token; i++)
+	{
+		tokens[i] = token;
+		token = _strtok(NULL, "\n ");
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
+}
